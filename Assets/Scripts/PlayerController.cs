251a593872ffc2f80d6fd560c391325fa73d776e
Abir M <102193884+abirmahmood6@users.noreply.github.com>
2025-12-02 07:16:07 -0500

    player movement setup

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb; //we set it private as we dont need to access this variable from other scripts
+    private float movementX;
+    private float movementY;
+    public float speed = 0; // public variable so we can access this from the inspector under playerScript
+
+    //a function is VOID when it does not return anything
+    void Start() //Start() runs once, right when the object appears in the game.
+    {
+        rb = GetComponent <Rigidbody>(); //this gets the rigidBody from the game object and sets to rb. If the rigidbody is on a PlayerObject, unity looks at the player, finds its rigidbody and sets to rb.
+    }
+    void OnMove(InputValue movementValue) //This function runs every time the player gives movement input.
+    {
+        // Vector2 == 2D. It create a Vector2 variable called movementVector and store a value in it
+         Vector2 movementVector = movementValue.Get<Vector2>();  // get the player's movement input as a 2D vector (x/y)
+         movementX = movementVector.x;
+         movementY = movementVector.y;
+    }
+    void FixedUpdate() // runs at a fixed rate (for physics) and used for applying Rigidbody force/movement
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed); // movement will be now based on the speed we set in inspector
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        // no need of update() function now
+    // }
+}
